2828d272f3a0c353ade8b40aefa77b00d1bc9fbb
the-Tiege <thomasjohndunne97@gmail.com>
2023-12-30 15:49:24 +0000

    add to head and tail methods added

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -1,22 +1,60 @@
 from node import Node
 
 class LinkedList:
-    def __init__(self, value, next_node=None):
-        self.head = Node(value=value, next_node=next_node)
-        self._length = 1
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self._length = 0
 
-    def add(self, value):
-        new_node = Node(value=value, next_node=self.head)
+    def add_to_head(self, value):
+        # Add to head of list
+        new_node = Node(value=value)
+        current_head = self.head
+
+        if current_head:
+            new_node.set_next_node(current_head)
+            current_head.set_prev_node(new_node)
+
+        if self.tail is None:
+            self.tail = current_head
+
         self.head = new_node
         self._length += 1
 
+    def add_to_tail(self, value):
+        new_node = Node(value=value)
+        current_tail = self.tail
+
+        if current_tail:
+            new_node.set_prev_node(current_tail)
+            current_tail.set_next_node(new_node)
+
+        if self.head is None:
+            self.head = new_node
+
+        self.tail = new_node
+        self._length += 1
+
+
+    def insert(self):
+        # Add to index
+        pass
+
     def remove(self):
+        # TODO: Remove from head of list
+        # TODO: Remove from tail of list
+        # TODO: Remove from middle of list
         pass
 
     def get(self):
+        # TODO: Traverse list
+        # TODO: Find element (index or value?)
+        # TODO: Return element
         pass
 
     def length(self):
+        # TODO: Update to traverse list and count elements. 
+        # TODO: Or make the current method more robust.
         return self._length
 
     def __repr__(self):
